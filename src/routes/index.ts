21cf3ed732df7ac61332bdaf398210ce5e8ca114
Lawal Hakeem <lawizyhal@gmail.com>
2022-02-26 20:38:24 +0100

    feat: added file-routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,9 @@
 import express, { Router } from "express";
 import authRoutes from "./auth";
-
+import fileRoutes from "./file";
 const router: Router = express.Router();
 
 router.use("/auth", authRoutes);
+router.use("/file", fileRoutes);
 
 export default router;
